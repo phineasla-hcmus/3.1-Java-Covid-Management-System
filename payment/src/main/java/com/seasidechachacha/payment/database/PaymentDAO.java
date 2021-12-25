18973308f7a77913502af8e20222a700f809b6a5
Nguyễn Thành Đạt <=>
2021-12-25 20:10:54 +0700

    update PaymentDAO (remove commit, rollback)

--- payment/src/main/java/com/seasidechachacha/payment/database/PaymentDAO.java
+++ payment/src/main/java/com/seasidechachacha/payment/database/PaymentDAO.java
@@ -18,24 +18,16 @@ public class PaymentDAO {
 	}
 
 	public static boolean addPaymentAcc(String userID, double amount) {
-		Connection c = DataSource.getConnection();
-		try {
-			c.setAutoCommit(false);
+		try (Connection c = DataSource.getConnection()) {
 			String query = "INSERT INTO transactionaccount(userID, balance) VALUES(?,?);";
 			PreparedStatement ps = c.prepareStatement(query);
 			ps.setString(1, userID);
 			ps.setDouble(2, amount);
 			boolean result = ps.executeUpdate() > 0;
-			c.commit();
 			DataSource.releaseConnection(c);
 			return result;
 		} catch (SQLException e) {
 			logger.error(e);
-			try {
-				c.rollback();
-			} catch (SQLException e1) {
-				logger.error(e1);
-			}
 		}
 		return false;
 	}

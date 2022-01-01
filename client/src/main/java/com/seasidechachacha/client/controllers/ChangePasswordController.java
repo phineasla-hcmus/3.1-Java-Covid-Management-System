d98a8c3f98ce4a0ddd40c8726030d0a4574e408c
Phineasla <63248715+Phineasla@users.noreply.github.com>
2022-01-01 10:05:57 +0700

    Update ChangePasswordController.java

--- client/src/main/java/com/seasidechachacha/client/controllers/ChangePasswordController.java
+++ client/src/main/java/com/seasidechachacha/client/controllers/ChangePasswordController.java
@@ -1,12 +1,18 @@
 package com.seasidechachacha.client.controllers;
 
+import com.seasidechachacha.client.database.UserDao;
 import com.seasidechachacha.client.global.Session;
+import com.seasidechachacha.client.global.TaskExecutor;
 import com.seasidechachacha.client.models.User;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class ChangePasswordController {
 
@@ -22,7 +28,44 @@ public class ChangePasswordController {
     @FXML
     private void initialize() {
         btnChangePassword.setOnAction(event -> {
-            // TODO
+            String currentPass = pfCurrentPass.getText();
+            String newPass = pfNewPass.getText();
+            String confirmPass = pfConfirmPass.getText();
+            if (!currentPass.isBlank()
+                    && !newPass.isBlank()
+                    && !confirmPass.isBlank()
+                    && newPass == confirmPass) {
+                changePasswordThread(currentPass, newPass);
+            }
         });
+    }
+
+    private void changePasswordThread(String currentPass, String newPass) {
+        Task<Boolean> changePasswordTask = new Task<Boolean>() {
+            @Override
+            public Boolean call() {
+                return UserDao.changePassword(Session.getUserId(), currentPass, newPass);
+            }
+        };
+
+        changePasswordTask.setOnSucceeded(ev -> {
+            Boolean result = changePasswordTask.getValue();
+            if (result == null) {
+                // How did we get here
+                logger.error("Bug in DAO changePassword");
+            } else {
+                resolveChangePassword(ev, result);
+            }
+        });
+
+        TaskExecutor.execute(changePasswordTask);
+    }
+
+    private void resolveChangePassword(WorkerStateEvent ev, boolean result) {
+        if (result) {
+            // Success
+        } else {
+            // Wrong oldPassword
+        }
     }
 }
